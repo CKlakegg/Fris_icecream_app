6c727b2d0b64f56e2f26f35545531a29e9ada3fb
CKlakegg <81484840+CKlakegg@users.noreply.github.com>
2021-05-11 00:14:51 +0200

    nytt fragment og icon

---
+++ app/src/main/java/com/example/fris_icecream_app/ui/main/database/OderDAO.kt
@@ -0,0 +1,8 @@
+package com.example.fris_icecream_app.ui.main.database
+
+import androidx.room.Dao
+
+
+@Dao
+class OderDAO {
+}
--- app/src/main/java/com/example/fris_icecream_app/ui/main/database/OrderObject.kt
+++ app/src/main/java/com/example/fris_icecream_app/ui/main/database/OrderObject.kt
@@ -1,11 +1,16 @@
 package com.example.fris_icecream_app.ui.main.database
 
 import androidx.room.Entity
-
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "order")
 class OrderObject (
     val price:Int,
     val type: String,
-    val howMany:Int)
+    val howMany:Int,
+    @PrimaryKey
+    val key:String,
+    val accessiblity: Double
+
+    )
